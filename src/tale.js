4c77cb4721a383c1d1e8aed1e21c18087436a416
Альберт Гаряев <albert@MacBook-Air-Albert.local>
2025-04-07 00:00:31 +0300

    functions

---
+++ src/tale.js
@@ -0,0 +1,25 @@
+function kolobok (name) {
+switch(name) {
+    case 'дедушка':
+        console.log ('Я от дедушки ушел');
+        break;
+    case 'заяц':
+        console.log ('Я от зайца ушел');
+        break;    
+    case 'лиса':
+        console.log ('Меня съели');
+        break    
+}
+}
+
+function newYear(character) {
+    const allowedNames = ['Дед мороз', 'Снегурочка'];
+    
+    if (!allowedNames.includes(character)) {
+      return 'Неизвестный новогодний персонаж!';
+    }
+    
+    return `${character}! ${character}! ${character}!`;
+  }
+
+  console.log (newYear('Снегурочка'))
